a7b0eaee1ae68a76eab2a511c8e0f86ecdb402dc
Jesus Ordosgoitty <jesuodz@gmail.com>
2023-07-26 14:27:16 -0400

    list coins v1

--- App.tsx
+++ App.tsx
@@ -1,19 +1,55 @@
+import React, { useState, useEffect } from 'react'
+import { 
+  FlatList, 
+  StyleSheet, 
+  Text, 
+  View, 
+  ListRenderItemInfo
+} from 'react-native';
+import coinsData from './data/coins.json'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+interface Coin {
+  id:     string;
+  symbol: string;
+  name:   string;
+}
 
 export default function App() {
+  const [coins, setCoins] = useState(coinsData)
+
+  const loadData = async () => {
+    try {
+      const res = await fetch('https://api.coingecko.com/api/v3/coins/listhttps://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en')
+
+      const data = await res.json()
+      setCoins(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const renderItem = (info: ListRenderItemInfo<Coin>) => {
+    return <Text style={{ color: '#fff'}}>{info.item.name}</Text>;
+  };
+
+  /* useEffect(() => {
+    loadData()
+  }, []) */
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        data={coins}
+        renderItem={renderItem}  
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#141414',
     alignItems: 'center',
     justifyContent: 'center',
   },
